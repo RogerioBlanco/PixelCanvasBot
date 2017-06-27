aa5f1bf44514d6d56767deb70612337a0d02bdba
RogerioBlanco <rogerio.b.nunes@gmail.com>
2017-06-27 15:26:54 +0100

    Fixing some bugs

--- pixel_canvas_bot.py
+++ pixel_canvas_bot.py
@@ -5,6 +5,9 @@ import requests
 import time
 import websocket
 import math
+import thread
+
+from random import randint
 from six.moves.urllib.parse import urlparse
 from argparse import ArgumentParser
 from struct import unpack_from
@@ -66,10 +69,20 @@ def post(url, payload, headers =
               'Referer': URL_BASE
               }
          ):
-    return requests.request('POST', url, data=payload, headers=headers, proxies = GLOBAL_PROXY)
+    response = requests.request('POST', url, data=payload, headers=headers, proxies = GLOBAL_PROXY)
+    
+    if response.status_code != 200:
+        raise Exception('Occurred an problem.\nMsg: ' + response.text)
+
+    return response
 
 def get(url, stream = False, headers = {'User-agent':'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'}):
-    return requests.get(url, stream=stream, headers=headers, proxies = GLOBAL_PROXY)
+    response = requests.get(url, stream=stream, headers=headers, proxies = GLOBAL_PROXY)
+    
+    if response.status_code != 200:
+        raise Exception('Occurred an problem.\nMsg: ' + response.text)
+    
+    return response
 
 def myself(fingerprint):
     return post(URL_BASE + 'api/me', '{"fingerprint":"%s"}' % fingerprint).json()
@@ -79,8 +92,12 @@ def send_pixel(x, y, color, fingerprint):
 
 def wait_time(data = {'waitSeconds':None}):
     if data['waitSeconds'] is not None:
-        print 'Waiting %s seconds' % str(data['waitSeconds'] + 1)
-        time.sleep(data['waitSeconds'] + 1)
+        wait = data['waitSeconds'] + randint(0, 9)
+        print 'Waiting %s seconds' % str(wait)
+        time.sleep(wait)
+
+def download_big_chuck(start_x, start_y, width, height):
+    return None
 
 def connect_websocket(fingerprint):
     def on_message(ws, message):
@@ -88,7 +105,7 @@ def connect_websocket(fingerprint):
             number = unpack_from('!H', message, 5)[0]
             x = unpack_from('!h', message, 1)[0] * MAGIC_NUMBER + ((number % MAGIC_NUMBER + MAGIC_NUMBER) % MAGIC_NUMBER)
             y = unpack_from('!h', message, 3)[0] * MAGIC_NUMBER + math.floor(number / MAGIC_NUMBER)
-            update_map(x, y, 15 & number)
+            update_map(int(x), int(y), int(15 & number))
             
     def on_error(ws, error):
         ws.close()
@@ -96,31 +113,47 @@ def connect_websocket(fingerprint):
     def on_close(ws):
         #TODO: reopen connetion
         print "### closed ###"
-        
+    
+    def on_open(ws):
+        print "Websocket open"
+
     url = get(URL_BASE + 'api/ws').json()['url']
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(url + '/?fingerprint=' + fingerprint, on_error = on_error, on_close = on_close)
+    ws = websocket.WebSocketApp(url + '/?fingerprint=' + fingerprint, on_error = on_error, on_close = on_close, on_open = on_open)
     ws.on_message = on_message
-    if GLOBAL_PROXY:
-        proxy = urlparse(GLOBAL_PROXY['http'])
-        ws.run_forever(http_proxy_host=proxy.hostname, http_proxy_port=proxy.port, http_proxy_auth=[proxy.username,proxy.password])        
-    else:
-        ws.run_forever()
+
+    def run(*args):
+        if GLOBAL_PROXY:
+            proxy = urlparse(GLOBAL_PROXY['http'])
+            proxy_auth = None
+            if proxy.username and proxy.password:
+                proxy_auth = [proxy.username, proxy.password]
+
+            ws.run_forever(http_proxy_host=proxy.hostname, http_proxy_port=proxy.port, http_proxy_auth=proxy_auth)
+        else:
+            ws.run_forever()
+
+    thread.start_new_thread(run, ())
 
 def load_image(file):
     im = Image.open(file).convert('RGB')
     width, height = im.size
     pix = im.load()
-    return {'width':width, 'height': height, 'image': pix}
+    return {'width':width, 'height': height, 'pix': pix}
 
-def draw_image(image, fingerprint, start_x, start_y):
-    for y in range(0, height):
-        for x in range(0, width):
-            color = COLORS_RGB[pix[x,y]]
-            if color:
-                print 'Pixel color %s in %s,%s' % (color, str(start_x + x),str(start_y + y))
-                return_p =  send_pixel(start_x + x, start_y + y, COLORS_INDEX[color], fingerprint)
-                wait_time(return_p)
+def draw_image(image, fingerprint, start_x, start_y, mode_defensive):
+    for y in range(0, image['height']):
+        for x in range(0, image['width']):
+            color = COLORS_RGB[image['pix'][x, y]]
+            if color and MAP_PIXELS[x][y] != COLORS_INDEX[color]:
+                response = send_pixel(start_x + x, start_y + y, COLORS_INDEX[color], fingerprint)
+                while not response['success']:
+                    wait_time(response)
+                    print 'Oh no, an error occurred. Trying again.'
+                    response = send_pixel(start_x + x, start_y + y, COLORS_INDEX[color], fingerprint)
+
+                wait_time(response)
+                update_map(start_x + x, start_y + y, COLORS_INDEX[color])
+                print 'Set pixel color %s in %s,%s' % (color, str(start_x + x), str(start_y + y))
 
 def setup_global_proxy(proxy_url, proxy_auth):
     global GLOBAL_PROXY 
@@ -131,7 +164,7 @@ def setup_global_proxy(proxy_url, proxy_auth):
 
 def coordinates_exist(x, y):
     try:
-        MAP_PIXELS[int(x)][int(y)]
+        MAP_PIXELS[x][y]
     except IndexError:
         return False
     return True
@@ -139,10 +172,10 @@ def coordinates_exist(x, y):
 def update_map(x, y, color):
     global MAP_PIXELS
     if coordinates_exist(x, y):
-        print "Updated %s %s" % (str(x),str(y))
+        print "Updated %s %s with %s index color" % (str(x), str(y), str(color))
         MAP_PIXELS[x][y] = color
 
-def setup_map(width, height, start_x, start_y):
+def setup_map(canvas, width, height, start_x, start_y):
     global MAP_PIXELS
     MAP_PIXELS = [[0 for y in range(start_y, start_y + height)] for x in range(start_x, start_x + width)]
 
@@ -154,10 +187,12 @@ def parse_args():
     parser.add_argument('-x','--start_x', required=True, type=int, dest='start_x', help='The point x axis that will start to draw')
     parser.add_argument('-y','--start_y', required=True, type=int, dest='start_y', help='The point y axis that will start to draw')
     parser.add_argument('--mode_defensive', required=False, default=True, dest='mode_defensive', help='Not implemented yet.')
-    parser.add_argument('--proxy_url', required=False, dest='proxy_url', help='Proxy  url. Ex: url:port')
-    parser.add_argument('--proxy_auth', required=False, dest='proxy_auth', help='Proxy auth. Ex: user:pass')
+    parser.add_argument('--proxy_url', required=False, dest='proxy_url', help='Proxy url with port. ex: url:port')
+    parser.add_argument('--proxy_auth', required=False, dest='proxy_auth', help='Proxy authentication. ex: user:pass')
 
     return parser.parse_args()
+
+
 if __name__ == '__main__':
     args = parse_args()
 
@@ -166,12 +201,14 @@ if __name__ == '__main__':
 
     image = load_image(args.file)
 
-    setup_map(image['width'], image['height'], args.start_x, args.start_y)
+    canvas = download_big_chuck(args.start_x, args.start_y, image['width'], image['height'])
+
+    setup_map(canvas, image['width'], image['height'], args.start_x, args.start_y)
 
     connect_websocket(args.fingerprint)
     
     myself = myself(args.fingerprint)
-    
+
     wait_time(myself)
     
     draw_image(image, args.fingerprint, args.start_x, args.start_y, args.mode_defensive)
