4b93a9bafb4af66f2654dadc7046f72cf14e9a3f
osman kayan <osmnkayan@gmail.com>
2017-08-01 19:20:11 +0200

    created converted images with image file md5 checksum code

--- src/image.py
+++ src/image.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from PIL import Image as pillow
+from colors import EnumColor
 import hashlib, os
 
 class Image(object):
@@ -12,10 +13,12 @@ class Image(object):
 
     def load_image(self, file):
 
-        if (os.path.isfile(self.checksum)):
+        if (os.path.isfile(os.getcwd() + '/img/.cache/' + self.checksum + '.jpg')):
             print self.checksum
         if (os.path.isfile(file)):
-            print file
+            print 'generatin Temp image for ' + os.getcwd() + '/img/.cache/' + self.checksum + '.jpg'
+            new_image = self.convert_pixels(pillow.open(file).convert('RGB'))
+            self.save_image(new_image, 'img/.cache/' + self.checksum + '.jpg')
 
         return pillow.open(file).convert('RGB')
 
@@ -25,3 +28,36 @@ class Image(object):
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
+
+    #refUrl: https://www.codementor.io/isaib.cicourel/image-manipulation-in-python-du1089j1u
+
+    def save_image(self, image, path):
+        image.save(path, 'jpeg')
+
+    # Create a new image with the given size
+    def create_image(self,i, j):
+        image = pillow.new("RGB", (i, j), "white")
+        return image
+
+    def get_pixel(self,image, i, j):
+        # Inside image bounds?
+        width, height = image.size
+        if i > width or j > height:
+            return None
+
+        pixel = image.getpixel((i, j))
+        return pixel
+
+    def convert_pixels(self, image):
+        width, height = image.size
+
+        new = self.create_image(width, height)
+        pixels = new.load()
+
+        for i in range(width):
+            for j in range(height):
+                pixel = self.get_pixel(image, i, j)
+                new_color = EnumColor.rgb(pixel, True, True)
+                pixels[i, j] = (int(new_color.rgb[0]), int(new_color.rgb[1]), int(new_color.rgb[2]))
+
+        return new
