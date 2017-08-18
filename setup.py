4fe58a45b02037d93a148e05d47a645022f44437
osman kayan <osmnkayan@gmail.com>
2017-08-18 11:18:34 +0200

    ready setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    import pip
+except ImportError, e:
+    print "installing pip"
+    os.system("get-pip.py")
+    import pip
+
+print "Checking required packages"
+pkgs = ['websocket-client', 'Pillow', 'requests']
+for package in pkgs:
+    try:
+        import package
+    except ImportError, e:
+        pip.main(['install', package])
+
+print "OK"
