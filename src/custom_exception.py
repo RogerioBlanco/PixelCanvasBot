af99e9faa10704ee1acf8b977532dea2a1fe819c
osman kayan <osmnkayan@gmail.com>
2017-12-07 17:58:12 +0200

    ReFormat coding style

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 class NeedUserInteraction(Exception):
-	def __init__(self, message):
-		super(Exception, self).__init__(message)
+    def __init__(self, message):
+        super(Exception, self).__init__(message)
