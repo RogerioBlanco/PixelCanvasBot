fc52c0078e00160b5e0fc944bbd4d118dae6f561
Pr0phet-of-Fear <49212464+Pr0phet-of-Fear@users.noreply.github.com>
2019-06-15 06:01:29 -0400

    Partial Fix
    Still not working, since Traanvas/Kekvas just stopped working; but I think that WAS what was wrong with it.

--- src/pixelcanvasio.py
+++ src/pixelcanvasio.py
@@ -11,7 +11,7 @@ from six.moves import range
 
 
 class PixelCanvasIO(object):
-    URL = 'http://pixelcanvas.io/'
+    URL = 'https://pixelcanvas.io/'
     HEADER_USER_AGENT = {'User-agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'}
     HEADERS = {
         'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
